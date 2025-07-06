11b787e1137b4e82ffff9451edc141eff1d1e139
Scott J Guyton <kiljoy001@gmail.com>
2025-07-06 01:15:26 -0700

    added agpl3 to ident algo

--- src/identity_algorithim.py
+++ src/identity_algorithim.py
@@ -1,3 +1,13 @@
+"""
+Brunnen-G Identity Algorithm
+Copyright (C) 2024 Scott Guyton
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+"""
+
 import asyncpg
 import hashlib
 from pqcrypto.sign.ml_dsa_65 import generate_keypair, sign, verify
